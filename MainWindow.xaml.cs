dd3d895ceac15544bf3e10dd072bf6fc105e81d7
blacksfk <blacksunflowakuttaspam@hotmail.com>
2022-06-23 21:57:24 +1000

    implemented main window code behind

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace SetupDiff {
 	public partial class MainWindow : Window {
-		public MainWindow() {
-			InitializeComponent();
+		// heirachy of directories and setup files in following structure:
+		// -> car dir
+		// 		-> track dir
+		// 			-> setup file (JSON)
+		// 			-> setup file (JSON)
+		// 		-> track dir
+		// 		...
+		// -> car dir
+		// etc.
+		private List<CarNode> tree;
+
+		// known cars (and their setup minimums)
+		private List<Car> cars;
+
+		// setups being compared
+		private ObservableCollection<Setup> setups;
+
+		public MainWindow(List<CarNode> tree, List<Car> cars) {
+			this.tree = tree;
+			this.cars = cars;
+			this.setups = new ObservableCollection<Setup>();
+			this.InitializeComponent();
+
+			// DataContext defaults to null so bind it to this instance
+			// in order to make use of its properties
+			this.DataContext = this;
+		}
+
+		public List<CarNode> Tree {
+			get {
+				return this.tree;
+			}
+		}
+
+		public ObservableCollection<Setup> Setups {
+			get {
+				return this.setups;
+			}
 		}
 	}
 }
