16661112d1bc53075786a6d698de7c63e4a7d6fe
blacksfk <blacksunflowakuttaspam@hotmail.com>
2022-06-23 20:54:14 +1000

    refactor: use public properties with defaults instead

--- Ref.cs
+++ Ref.cs
@@ -3,30 +3,16 @@ namespace SetupDiff {
 	// Eg.: Pressure minimum = 20.3, therefore:
 	// start = 203, factor = 10, increment = 1
 	public class Ref {
-		private int start = 0;
-		private int factor = 1;
-		private int increment = 1;
-
-		public Ref() {}
-
-		public Ref(int start) {
-			this.start = start;
-		}
-
-		public Ref(int start, int factor) : this(start) {
-			this.factor = factor;
-		}
-
-		public Ref(int start, int factor, int inc) : this(start, factor) {
-			this.increment = inc;
-		}
+		public int Start {get;set;} = 0;
+		public int Factor {get;set;} = 1;
+		public int Increment {get;set;} = 1;
 
 		public int Calculate(int offset) {
-			return this.start + offset * this.increment;
+			return this.Start + offset * this.Increment;
 		}
 
 		public float CalculateFloat(int offset) {
-			return this.Calculate(offset) / (float) this.factor;
+			return this.Calculate(offset) / (float) this.Factor;
 		}
 	}
 }
