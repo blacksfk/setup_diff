7de8088af7c861e04149b4d3c6ccffd0a636273d
blacksfk <blacksunflowakuttaspam@hotmail.com>
2022-07-14 20:17:19 +1000

    use constants instead of an enum

--- SetupCollection.cs
+++ SetupCollection.cs
@@ -8,6 +8,16 @@ namespace SetupDiff {
 	// Wrapper where each member is an observable collection of setup
 	// values.
 	public class SetupCollection {
+		// end
+		private const int F = 0;
+		private const int R = 1;
+
+		// corner
+		private const int FL = 0;
+		private const int FR = 1;
+		private const int RL = 2;
+		private const int RR = 3;
+
 		public OCStr CarName {get;set;} = new OCStr();
 		public OCStr SetupName {get;set;} = new OCStr();
 
@@ -119,85 +129,85 @@ namespace SetupDiff {
 			this.CarName.Add(car.Name);
 			this.SetupName.Add(s.SetupName);
 
-			this.PressureFL.Add(car.Pressure.Apply(tp[Corner.FL]));
-			this.PressureFR.Add(car.Pressure.Apply(tp[Corner.FR]));
-			this.PressureRL.Add(car.Pressure.Apply(tp[Corner.RL]));
-			this.PressureRR.Add(car.Pressure.Apply(tp[Corner.RR]));
+			this.PressureFL.Add(car.Pressure.Apply(tp[FL]));
+			this.PressureFR.Add(car.Pressure.Apply(tp[FR]));
+			this.PressureRL.Add(car.Pressure.Apply(tp[RL]));
+			this.PressureRR.Add(car.Pressure.Apply(tp[RR]));
 
-			this.ToeFL.Add(car.Toe[End.F].Apply(toe[Corner.FL]));
-			this.ToeFR.Add(car.Toe[End.F].Apply(toe[Corner.FR]));
-			this.ToeRL.Add(car.Toe[End.R].Apply(toe[Corner.RL]));
-			this.ToeRR.Add(car.Toe[End.R].Apply(toe[Corner.RR]));
+			this.ToeFL.Add(car.Toe[F].Apply(toe[FL]));
+			this.ToeFR.Add(car.Toe[F].Apply(toe[FR]));
+			this.ToeRL.Add(car.Toe[R].Apply(toe[RL]));
+			this.ToeRR.Add(car.Toe[R].Apply(toe[RR]));
 
-			this.CamberFL.Add(car.Camber[End.F].Apply(cbr[Corner.FL]));
-			this.CamberFR.Add(car.Camber[End.F].Apply(cbr[Corner.FR]));
-			this.CamberRL.Add(car.Camber[End.R].Apply(cbr[Corner.RL]));
-			this.CamberRR.Add(car.Camber[End.R].Apply(cbr[Corner.RR]));
+			this.CamberFL.Add(car.Camber[F].Apply(cbr[FL]));
+			this.CamberFR.Add(car.Camber[F].Apply(cbr[FR]));
+			this.CamberRL.Add(car.Camber[R].Apply(cbr[RL]));
+			this.CamberRR.Add(car.Camber[R].Apply(cbr[RR]));
 
-			this.CasterFL.Add(car.Caster.Apply(s.Align.CasterLF[Corner.FL]));
-			this.CasterFR.Add(car.Caster.Apply(s.Align.CasterLF[Corner.FR]));
+			this.CasterFL.Add(car.Caster.Apply(s.Align.CasterLF));
+			this.CasterFR.Add(car.Caster.Apply(s.Align.CasterRF));
 
 			this.TC1.Add(car.TC1.Apply(s.Elec.TC1));
 			this.TC2.Add(car.TC2.Apply(s.Elec.TC2));
 			this.ABS.Add(car.ABS.Apply(s.Elec.ABS));
-			this.ECU.Add(car.ECU.Apply(s.Elec.ECU));
+			this.ECU.Add(car.ECU.Apply(s.Elec.ECUMap));
 
 			// strategy TODO: apply the correct compound
 			this.Fuel.Add(car.Fuel.Apply(s.Strat.Fuel));
 			this.Tyre.Add(car.Tyre.Apply(s.Strat.TyreSet));
-			this.PadF.Add(car.PadF.Apply(s.Strat.FrontBrakePadCompound));
-			this.PadR.Add(car.PadR.Apply(s.Strat.RearBrakePadCompound));
+			this.PadF.Add(car.Pads.Apply(s.Strat.FrontBrakePadCompound));
+			this.PadR.Add(car.Pads.Apply(s.Strat.RearBrakePadCompound));
 
 			// mechanical
 			this.SteeringRatio.Add(car.SteeringRatio.Apply(s.Align.SteerRatio));
-			this.ARBF.Add(car.ARBF.Apply(s.Mech.ARBFront));
+			this.ARBF.Add(car.ARB.Apply(s.Mech.ARBFront));
 			this.BrakePower.Add(car.BrakePower.Apply(s.Mech.BrakeTorque));
 			this.BrakeBias.Add(car.BrakeBias.Apply(s.Mech.BrakeBias));
-			this.ARBR.Add(car.ARBR.Apply(s.Mech.ARBRear));
+			this.ARBR.Add(car.ARB.Apply(s.Mech.ARBRear));
 			this.Preload.Add(car.Preload.Apply(s.Preload));
 
-			this.WheelRateFL.Add(car.WheelRate[End.F].Apply(s.Mech.WheelRate[Corner.FL]));
-			this.WheelRateFR.Add(car.WheelRate[End.F].Apply(s.Mech.WheelRate[Corner.FR]));
-			this.WheelRateRL.Add(car.WheelRate[End.R].Apply(s.Mech.WheelRate[Corner.RL]));
-			this.WheelRateRR.Add(car.WheelRate[End.R].Apply(s.Mech.WheelRate[Corner.RR]));
+			this.WheelRateFL.Add(car.WheelRate[F].Apply(s.Mech.WheelRate[FL]));
+			this.WheelRateFR.Add(car.WheelRate[F].Apply(s.Mech.WheelRate[FR]));
+			this.WheelRateRL.Add(car.WheelRate[R].Apply(s.Mech.WheelRate[RL]));
+			this.WheelRateRR.Add(car.WheelRate[R].Apply(s.Mech.WheelRate[RR]));
 
-			this.BumpStopRateFL.Add(car.BumpStopRate[End.F].Apply(bsr[Corner.FL]));
-			this.BumpStopRateFR.Add(car.BumpStopRate[End.F].Apply(bsr[Corner.FR]));
-			this.BumpStopRateRL.Add(car.BumpStopRate[End.R].Apply(bsr[Corner.RL]));
-			this.BumpStopRateRR.Add(car.BumpStopRate[End.R].Apply(bsr[Corner.RR]));
+			this.BumpStopRateFL.Add(car.BumpStopRate[F].Apply(bsr[FL]));
+			this.BumpStopRateFR.Add(car.BumpStopRate[F].Apply(bsr[FR]));
+			this.BumpStopRateRL.Add(car.BumpStopRate[R].Apply(bsr[RL]));
+			this.BumpStopRateRR.Add(car.BumpStopRate[R].Apply(bsr[RR]));
 
-			this.BumpStopRangeFL.Add(car.BumpStopRange.Apply(bsw[Corner.FL]));
-			this.BumpStopRangeFR.Add(car.BumpStopRange.Apply(bsw[Corner.FR]));
-			this.BumpStopRangeRL.Add(car.BumpStopRange.Apply(bsw[Corner.RL]));
-			this.BumpStopRangeRR.Add(car.BumpStopRange.Apply(bsw[Corner.RR]));
+			this.BumpStopRangeFL.Add(car.BumpStopRange.Apply(bsw[FL]));
+			this.BumpStopRangeFR.Add(car.BumpStopRange.Apply(bsw[FR]));
+			this.BumpStopRangeRL.Add(car.BumpStopRange.Apply(bsw[RL]));
+			this.BumpStopRangeRR.Add(car.BumpStopRange.Apply(bsw[RR]));
 
-			this.BumpSlowFL.Add(car.BumpSlow.Apply(s.Dampers.BumpSlow[Corner.FL]));
-			this.BumpSlowFR.Add(car.BumpSlow.Apply(s.Dampers.BumpSlow[Corner.FR]));
-			this.BumpSlowRL.Add(car.BumpSlow.Apply(s.Dampers.BumpSlow[Corner.RL]));
-			this.BumpSlowRR.Add(car.BumpSlow.Apply(s.Dampers.BumpSlow[Corner.RR]));
+			this.BumpSlowFL.Add(car.BumpSlow.Apply(s.Dampers.BumpSlow[FL]));
+			this.BumpSlowFR.Add(car.BumpSlow.Apply(s.Dampers.BumpSlow[FR]));
+			this.BumpSlowRL.Add(car.BumpSlow.Apply(s.Dampers.BumpSlow[RL]));
+			this.BumpSlowRR.Add(car.BumpSlow.Apply(s.Dampers.BumpSlow[RR]));
 
-			this.BumpFastFL.Add(car.BumpFast.Apply(s.Dampers.BumpFast[Corner.FL]));
-			this.BumpFastFR.Add(car.BumpFast.Apply(s.Dampers.BumpFast[Corner.FR]));
-			this.BumpFastRL.Add(car.BumpFast.Apply(s.Dampers.BumpFast[Corner.RL]));
-			this.BumpFastRR.Add(car.BumpFast.Apply(s.Dampers.BumpFast[Corner.RR]));
+			this.BumpFastFL.Add(car.BumpFast.Apply(s.Dampers.BumpFast[FL]));
+			this.BumpFastFR.Add(car.BumpFast.Apply(s.Dampers.BumpFast[FR]));
+			this.BumpFastRL.Add(car.BumpFast.Apply(s.Dampers.BumpFast[RL]));
+			this.BumpFastRR.Add(car.BumpFast.Apply(s.Dampers.BumpFast[RR]));
 
-			this.ReboundSlowFL.Add(car.ReboundSlow.Apply(s.Dampers.ReboundSlow[Corner.FL]));
-			this.ReboundSlowFR.Add(car.ReboundSlow.Apply(s.Dampers.ReboundSlow[Corner.FR]));
-			this.ReboundSlowRL.Add(car.ReboundSlow.Apply(s.Dampers.ReboundSlow[Corner.RL]));
-			this.ReboundSlowRR.Add(car.ReboundSlow.Apply(s.Dampers.ReboundSlow[Corner.RR]));
+			this.ReboundSlowFL.Add(car.ReboundSlow.Apply(s.Dampers.ReboundSlow[FL]));
+			this.ReboundSlowFR.Add(car.ReboundSlow.Apply(s.Dampers.ReboundSlow[FR]));
+			this.ReboundSlowRL.Add(car.ReboundSlow.Apply(s.Dampers.ReboundSlow[RL]));
+			this.ReboundSlowRR.Add(car.ReboundSlow.Apply(s.Dampers.ReboundSlow[RR]));
 
-			this.ReboundFastFL.Add(car.ReboundFast.Apply(s.Dampers.ReboundFast[Corner.FL]));
-			this.ReboundFastFR.Add(car.ReboundFast.Apply(s.Dampers.ReboundFast[Corner.FR]));
-			this.ReboundFastRL.Add(car.ReboundFast.Apply(s.Dampers.ReboundFast[Corner.RL]));
-			this.ReboundFastRR.Add(car.ReboundFast.Apply(s.Dampers.ReboundFast[Corner.RR]));
+			this.ReboundFastFL.Add(car.ReboundFast.Apply(s.Dampers.ReboundFast[FL]));
+			this.ReboundFastFR.Add(car.ReboundFast.Apply(s.Dampers.ReboundFast[FR]));
+			this.ReboundFastRL.Add(car.ReboundFast.Apply(s.Dampers.ReboundFast[RL]));
+			this.ReboundFastRR.Add(car.ReboundFast.Apply(s.Dampers.ReboundFast[RR]));
 
 			// aero
-			this.RideHeightF.Add(car.RideHeight[End.F].Apply(s.Aero.RideHeight[End.F]));
+			this.RideHeightF.Add(car.RideHeight[F].Apply(s.Aero.RideHeight[F]));
 			this.Splitter.Add(car.Splitter.Apply(s.Aero.Splitter));
-			this.DuctF.Add(car.Duct.Apply(s.Aero.BrakeDuct[End.F]));
-			this.RideHeightR.Add(car.RideHeight[End.R].Apply(s.Aero.RideHeight[End.R]));
+			this.DuctF.Add(car.Duct.Apply(s.Aero.BrakeDuct[F]));
+			this.RideHeightR.Add(car.RideHeight[R].Apply(s.Aero.RideHeight[R]));
 			this.Wing.Add(car.Wing.Apply(s.Aero.RearWing));
-			this.DuctR.Add(car.Duct.Apply(s.Aero.BrakeDuct[End.R]));
+			this.DuctR.Add(car.Duct.Apply(s.Aero.BrakeDuct[R]));
 		}
 	}
 }
