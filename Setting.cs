77c285b111cb6307c26d7b86a660bd561185057a
blacksfk <blacksunflowakuttaspam@hotmail.com>
2022-07-15 16:59:05 +1000

    refactor: Diff now returns an int instead of a boolean

--- Setting.cs
+++ Setting.cs
@@ -18,11 +18,12 @@ namespace SetupDiff {
 			}
 		}
 
-		// Compare this setting against another. Returns false if they're
-		// identical and true otherwise. I.e. there's difference between
-		// the values if true is returned.
-		public bool Diff(Setting s) {
-			return this.offset != s.offset;
+		// Compare this setting against another. Returns:
+		// < 0 if this setting is less than the provided
+		// > 0 if this setting is greater than the provided
+		// 0 if they're identical
+		public int Diff(Setting s) {
+			return this.offset - s.offset;
 		}
 	}
 }
