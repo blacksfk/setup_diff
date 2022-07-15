ea89fe1c1d675099c8b3d889b1e6994f955e6d80
blacksfk <blacksunflowakuttaspam@hotmail.com>
2022-07-15 21:57:21 +1000

    created event handlers for adding and removing setups

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -45,5 +45,57 @@ namespace SetupDiff {
 				return this.setups;
 			}
 		}
+
+		// TreeView setup node click event handler. Adds the clicked
+		// setup to the collection for comparison.
+		private void addSetup(object sender, MouseButtonEventArgs args) {
+			if (args.ClickCount < 2) {
+				// double clicks only please
+				return;
+			}
+
+			// assert the sender as a SetupTreeNode and try to load the setup
+			SetupTreeNode stn = (SetupTreeNode) sender;
+			Setup setup;
+
+			try {
+				setup = Setup.FromFile(stn.FilePath);
+			} catch (Exception ex) {
+				MessageBox.Show(string.Format("Could not load setup: {0}", ex.Message));
+
+				return;
+			}
+
+			// match the setup to a known car
+			Car? car = null;
+
+			foreach (Car c in this.cars) {
+				if (c.ID == setup.CarName) {
+					car = c;
+					break;
+				}
+			}
+
+			if (car == null) {
+				// car not found
+				MessageBox.Show(string.Format(
+					"Car: {0} is not currently supported", setup.CarName));
+
+				return;
+			}
+
+			// add it to the collection
+			this.setups.Append(setup, car);
+		}
+
+		// Setup column "X" button event handler. Removes the setup
+		// specified in the button's tag from the comparison.
+		private void removeSetup(object sender, RoutedEventArgs e) {
+			string path = (string) ((Button) sender).Tag;
+
+			if (!this.setups.Remove(path)) {
+				MessageBox.Show(string.Format("Could not remove setup: {0}", path));
+			}
+		}
 	}
 }
