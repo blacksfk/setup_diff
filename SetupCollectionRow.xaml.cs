5872259234ffcd08f7786cd6536140698cec1b69
blacksfk <blacksunflowakuttaspam@hotmail.com>
2022-07-15 21:52:52 +1000

    usercontrol that wraps setup collection row elements

---
+++ SetupCollectionRow.xaml.cs
@@ -0,0 +1,63 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+
+namespace SetupDiff {
+	public partial class SetupCollectionRow : UserControl {
+		public static readonly DependencyProperty HeaderProp =
+			DependencyProperty.Register(
+				"Header",
+				typeof (string),
+				typeof (SetupCollectionRow),
+				new FrameworkPropertyMetadata(
+					string.Empty));
+
+		public static readonly DependencyProperty ValsProp =
+			DependencyProperty.Register(
+				"Values",
+				typeof (ObservableCollection<Setting>),
+				typeof (SetupCollectionRow),
+				new FrameworkPropertyMetadata(
+					new ObservableCollection<Setting>()));
+
+		public static readonly DependencyProperty CellWidthProp =
+			DependencyProperty.Register(
+				"CellWidth",
+				typeof (int),
+				typeof (SetupCollectionRow),
+				new FrameworkPropertyMetadata(
+					200));
+
+		public SetupCollectionRow() {
+			this.InitializeComponent();
+			this.sp.DataContext = this;
+		}
+
+		public string Header {
+			get {
+				return (string) GetValue(HeaderProp);
+			}
+			set {
+				SetValue(HeaderProp, value);
+			}
+		}
+
+		public ObservableCollection<Setting> Values {
+			get {
+				return (ObservableCollection<Setting>) GetValue(ValsProp);
+			}
+			set {
+				SetValue(ValsProp, value);
+			}
+		}
+
+		public int CellWidth {
+			get {
+				return (int) GetValue(CellWidthProp);
+			}
+			set {
+				SetValue(CellWidthProp, value);
+			}
+		}
+	}
+}
